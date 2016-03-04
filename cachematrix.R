096be435b31cb007219dd324d28b8fb5872377cf
magicworld168 <magicworld168@users.noreply.github.com>
2016-03-03 20:33:18 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,20 +1,18 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
 
+## Creating the inverse of a matrix
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+  m <- NULL  # sets initial inverse of x to NULL
   set <- function(y) {
     x <<- y
-    m <<- NULL
+    m <<- NULL  #set the inverse matrix to null when input is reset
   }
-  get <- function() x
+  get <- function() x  ##retreive matrix
   
-  setInverse <- function(solve) m <<- solve
-  getInverse <- function() m
+  setInverse <- function(solve) m <<- solve  ##inverse the matrix
+  getInverse <- function() m   ##retreive inverse matrix
   
-  list(set = set, get = get,
+  list(set = set, get = get,   # list of functions to be returned
        setInverse  = setInverse ,
        getInverse = getInverse )
 }
@@ -24,16 +22,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## Write a short comment describing this function
-
+## Caching the inverse matrix.  If the inverse exist, take the inverse from cache. 
 cacheSolve <- function(x, ...) {
   m <- x$getInverse()
-  if(!is.null(m)) {
+  if(!is.null(m)) {     ## if an inverse matrix exists, return the inverse and notify users
     message("getting cached data")
     return(m)
   }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setInverse(m)
-  m
+  data <- x$get()  ## retreive matrix
+  m <- solve(data, ...)  ##inverse the matrix
+  x$setInverse(m)   ## cache the inverse matrix
+  m  #return inverse
 }
